f713af54d9102d4d51d4bc9007b57bab541b6198
Usuario <Usuario@DESKTOP-V8DI44A>
2020-10-31 00:05:42 -0600

    se creo MaxNum y se borro Prueba2

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,14 +1,33 @@
 
 import java.util.Scanner;
 
-
-
-
-
 public class Prueba {
 
-
+    public static void maxNum(int[] a) {
+        
+        int base = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > base) {
+                base = a[i];
+            }
+        }
+    System.out.println("El numero maximo es "+base);
+    }
     
+    public static void main(String[] args){
+         
+        Scanner entrada=new Scanner(System.in);
+        System.out.println("¿Cuantos numeros desea comparar?");
+        int num=entrada.nextInt();
+        int[] numA=new int[num];
+        int a;
+        for(int i=0;i<num;i++){
+            System.out.println("Digite numero "+(i+1));
+            numA[i]=entrada.nextInt();
+        }
+        
+        maxNum(numA);
+    
+    }
+
 }
-
-
