0b8103c9524b0421ce8e2134737bf4d295af07fc
Usuario <Usuario@DESKTOP-V8DI44A>
2020-11-04 20:18:49 -0600

    se creo la clase CalcularImpuestos

--- src/Prueba2.java
+++ src/Prueba2.java
@@ -1,33 +1,119 @@
 
-import java.util.Arrays;
 import java.util.Scanner;
-
-
-
 
 
 public class Prueba2 {
  
-    public static void Reordenar(int[] arreglo){
-        
-        
-        for(int i=(arreglo.length-1);0<=i;i--){
-            System.out.println(arreglo[i]);
-        }
-    }
+    public static double calcularImpuestos (int dependiente, double salario){
     
-    public static void main(String[] args){
-        System.out.println("¿Cuantos digitos desea invertir?");
-        Scanner entrada = new Scanner(System.in);
-        int base = entrada.nextInt();
-        int[] arreglo = new int[base]; 
+
+        double porcentaje;
+        double IR = 0;
+        double resultado;
         
-        for(int i = 0;i<arreglo.length;i++){
-            System.out.println("digite el numero "+(i+1));
-            arreglo[i]=entrada.nextInt();
+        switch (dependiente){
+            case 1:
+                porcentaje=salario;
+                if(salario<200000){
+                    IR=salario;
+                }else if(200000<=salario&&salario<400000){
+                    IR=porcentaje*0.10;
+                }else if(400000<=salario&&salario<600000){
+                    IR=porcentaje*0.15;
+                }else if(600000<=salario&&salario<800000){
+                    IR=porcentaje*0.20;
+                }else if(salario>=800000){
+                    IR=porcentaje*0.30;
+                }
+                resultado=IR;
+                System.out.println(resultado);
+                break;
+            case 2:
+                porcentaje=salario*0.90;
+                if(salario<200000){
+                    IR=salario;
+                }else if(200000<=salario&&salario<400000){
+                    IR=porcentaje*0.10;
+                }else if(400000<=salario&&salario<600000){
+                    IR=porcentaje*0.15;
+                }else if(600000<=salario&&salario<800000){
+                    IR=porcentaje*0.20;
+                }else if(salario>=800000){
+                    IR=porcentaje*0.30;
+                }
+                resultado=IR;
+                System.out.println(resultado);
+                break;
+            case 3:
+                porcentaje=salario*0.70;
+                if(salario<200000){
+                    IR=salario;
+                }else if(200000<=salario&&salario<400000){
+                    IR=porcentaje*0.10;
+                }else if(400000<=salario&&salario<600000){
+                    IR=porcentaje*0.15;
+                }else if(600000<=salario&&salario<800000){
+                    IR=porcentaje*0.20;
+                }else if(salario>=800000){
+                    IR=porcentaje*0.30;
+                }
+                resultado=IR;
+                System.out.println(resultado);
+                break;
+            case 4:
+                porcentaje=salario*0.60;
+                if(salario<200000){
+                    IR=salario;
+                }else if(200000<=salario&&salario<400000){
+                    IR=porcentaje*0.10;
+                }else if(400000<=salario&&salario<600000){
+                    IR=porcentaje*0.15;
+                }else if(600000<=salario&&salario<800000){
+                    IR=porcentaje*0.20;
+                }else if(salario>=800000){
+                    IR=porcentaje*0.30;
+                }
+                resultado=IR;
+                System.out.println(resultado);
+                break;                
+            default:
+                porcentaje=salario*0.40;
+                if(salario<200000){
+                    IR=salario;
+                }else if(200000<=salario&&salario<400000){
+                    IR=porcentaje*0.10;
+                }else if(400000<=salario&&salario<600000){
+                    IR=porcentaje*0.15;
+                }else if(600000<=salario&&salario<800000){
+                    IR=porcentaje*0.20;
+                }else if(salario>=800000){
+                    IR=porcentaje*0.30;
+                }
+                resultado=IR;
+                System.out.println(resultado);
+                break;                  
         }
-       System.out.println("De los numeros que digito "+Arrays.toString(arreglo)+" el resultado es la siguiente:"); 
-        Reordenar(arreglo);
+        return resultado;
+    }
+
+    public static void main(String[] args){
+        Scanner entrada=new Scanner(System.in);
+        int dependiente;
+        double salario;
         
+        System.out.println("¡Bienvenido al sistema de impuestos sobre la renta!");
+        System.out.println();
+
+        
+        System.out.println("Por favor ingrese cuantas personas son:");
+        dependiente=entrada.nextInt();
+        System.out.println();
+
+        System.out.println("¿Cuanto es su salario mensual?:");
+        salario=entrada.nextDouble();
+        System.out.println();
+        
+        System.out.println("Su pago de los impuestos sobre la renta es:");
+        calcularImpuestos(dependiente, salario);
     }
 }
