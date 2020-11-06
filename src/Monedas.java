b61772209598629843e5d581a0031d5fc4ff08b2
Usuario <Usuario@DESKTOP-V8DI44A>
2020-11-05 21:58:43 -0600

    se creo la clase Monedas

--- src/Monedas.java
+++ src/Monedas.java
@@ -1,15 +1,14 @@
 
 import java.util.Scanner;
 
-public class Prueba2 {
+public class Monedas {
 
     public static int monedas(int num) {
 
         int monedas[] = {1000, 500, 100, 10, 5, 1};
         int cantidad[] = {0, 0, 0, 0, 0, 0};
         cantidad[0] = num;
-        
-        
+
         for (int i = 0; i < 1; i++) {
 
             while (cantidad[i] > 0 && cantidad[i] != num - 1) {
@@ -24,23 +23,23 @@ public class Prueba2 {
                 }
                 break;
             }
-            int total=0;
-            for(int conteo=0;conteo<cantidad.length;conteo++){
-                total+=cantidad[conteo];
+            int total = 0;
+            for (int conteo = 0; conteo < cantidad.length; conteo++) {
+                total += cantidad[conteo];
             }
-            System.out.println("El total de monedas es "+total);
+            System.out.println("El total de monedas es " + total);
         }
         return num;
     }
-    
-    public static void main(String[] args){
 
-        Scanner entrada=new Scanner(System.in);
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
         int num;
         System.out.println("Ingresa un Numero entero:");
-        num=entrada.nextInt();
+        num = entrada.nextInt();
         System.out.println();
-        System.out.println("La cantida "+num+" se distribuye en monedas:");
+        System.out.println("La cantida " + num + " se distribuye en monedas:");
         monedas(num);
     }
 
